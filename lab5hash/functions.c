02099c86da304ecb6b5fe9ce07fc6a7603f2e637
Pau1oo <bukato_pavel@mail.ru>
2023-05-24 22:59:35 +0300

    1

--- lab5hash/functions.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by bukat on 22.05.2023.
-//
-
-#include "functions.h"
--- lab5hash/functions.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by bukat on 22.05.2023.
-//
-
-#ifndef LAB5HASH_FUNCTIONS_H
-#define LAB5HASH_FUNCTIONS_H
-
-#endif //LAB5HASH_FUNCTIONS_H
